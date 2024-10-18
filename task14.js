3990726659de422c3388e96286e337a9e628a1fe
Liuba <1990yavorska@gmail.com>
2024-10-18 17:43:48 +0200

    fix: reverse array without changing original array in task14.js

--- task14.js
+++ task14.js
@@ -1,14 +1,25 @@
 // Write a function that reverses an array without modifying the original array.
 
+const deepCopy = (obj) => {
+  if (typeof obj !== "object" || obj === null) {
+    return obj;
+  }
+
+  const copiedObj = Array.isArray(obj) ? [] : {};
+
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      copiedObj[key] = deepCopy(obj[key]);
+    }
+  }
+
+  return copiedObj;
+};
+
 function reverseArray(arr) {
-  //   ES6;
-  return [...arr].reverse();
+  const copiedArray = arr.map((item) => deepCopy(item));
 
-  //   ES5;
-  //   return arr.slice().reverse();
-
-  //   toReversed()
-  //   return arr.toReversed();
+  return copiedArray.reverse();
 }
 
 reverseArray([1, 2, 3]); // [3, 2, 1]
