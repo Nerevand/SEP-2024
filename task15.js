71050b225c1cb2acb7091b2141f8d46f6d8dc8e7
Liuba <1990yavorska@gmail.com>
2024-10-18 16:15:37 +0200

    fix: return convertedValue instead of the passed one in function in task15.js

--- task15.js
+++ task15.js
@@ -1,8 +1,9 @@
 // Write a function that takes a string and converts it to a number. If the conversion is not possible, return null.
 
 function convertToNumber(str) {
-  const convertedStr = isNaN(Number(str)) ? null : str;
-  return convertedStr;
+  const convertedValue = Number(str);
+
+  return isNaN(convertedValue) ? null : convertedValue;
 }
 
 convertToNumber("42"); // 42
