e4e51a0c59679a6913a622173d3e9d96c2c92223
Liuba <1990yavorska@gmail.com>
2024-10-18 15:46:55 +0200

    fix: handle check for empty array and array with values that are not numbers in task10.js

--- task10.js
+++ task10.js
@@ -2,10 +2,30 @@
 
 function getAverage(arr) {
   //   ES6
+  if (!arr.length) {
+    return "Array is empty.";
+  }
+
+  if (arr.some((item) => typeof item !== "number")) {
+    return "Can't calculate the average. Some of elements are not numbers.";
+  }
+
   const sum = arr.reduce((acc, item) => acc + item, 0);
   return sum / arr.length;
 
   //   ES5
+  // const isNumberArr = arr.some(function (item) {
+  //   return typeof item !== "number";
+  // });
+
+  // if (!arr.length) {
+  //    return "Array is empty.";
+  // }
+
+  // if (isNumberArr) {
+  //   return "Can't calculate the average. Some of elements are not numbers.";
+  // }
+
   //   var sum = 0;
   //   for (var i = 0; i < arr.length; i++) {
   //     sum += arr[i];
