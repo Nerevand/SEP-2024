5fcf0dee497c3d8542557426dea1fd01ff2eb8c7
Liuba <1990yavorska@gmail.com>
2024-10-28 12:30:39 +0100

    fix: add check for undefined values in task12.js

--- task12.js
+++ task12.js
@@ -1,7 +1,7 @@
 // Write a function swap(a, b) that swaps two variables using array destructuring and return that variables
 
 const swap = (a, b) => {
-  if (a && b) {
+  if (a !== undefined && b !== undefined) {
     [a, b] = [b, a];
     return [a, b];
   }
