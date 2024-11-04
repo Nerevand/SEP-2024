d84993bc31502662f50dc0d25f985e161cb5bad5
Liuba <1990yavorska@gmail.com>
2024-11-04 16:36:04 +0100

    fix: add checks for different spiral matrix arrays to be unraveled in task1.js

--- task1.js
+++ task1.js
@@ -6,6 +6,14 @@
 //  [10, 9, 8, 7]    // ↑ ← ← ↲
 //  ];
 
+// const matrixArr = [
+//   [1, 2, 3, 4, 5],
+//   [16, 17, 18, 19, 6],
+//   [15, 24, 25, 20, 7],
+//   [14, 23, 22, 21, 8],
+//   [13, 12, 11, 10, 9],
+// ];
+
 //  [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 //  NOTE: initial array should stay the same
 
@@ -32,13 +40,16 @@ function unravelArr(arr) {
   const copiedArr = structuredClone(arr);
   let unraveled = [];
 
-  while (copiedArr.length > 0) {
-    let [top, right, bottom, left] = copiedArr;
-
-    top = copiedArr.shift();
-    right = copiedArr.map((row) => row.pop());
-    bottom = copiedArr.pop().reverse();
-    left = copiedArr.map((row) => row.shift()).reverse();
+  while (copiedArr.length) {
+    const top = copiedArr.shift();
+    const right = copiedArr
+      .map((row) => row.pop())
+      .filter((numItem) => numItem !== undefined);
+    const bottom = copiedArr.length ? copiedArr.pop().reverse() : [];
+    const left = copiedArr
+      .map((row) => row.shift())
+      .reverse()
+      .filter((numItem) => numItem !== undefined);
 
     unraveled = [...unraveled, ...top, ...right, ...bottom, ...left];
   }
