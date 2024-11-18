773f4f1d8e35f21fe0723b893de1ac5a4b07180a
Liuba <1990yavorska@gmail.com>
2024-11-18 17:46:11 +0100

    fix: show current year

--- src/components/common/Footer/Copyright/Copyright.jsx
+++ src/components/common/Footer/Copyright/Copyright.jsx
@@ -3,10 +3,12 @@ import { Typography } from "@mui/material";
 import "./style.css";
 
 const Copyright = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="copyright">
       <Typography variant="copyright" component="p" className="copyright-text">
-        &copy; Simple Art, Inc. 2021
+        &copy; Simple Art, Inc. {currentYear}
       </Typography>
       <Typography variant="copyright" component="p" className="copyright-text">
         All rights reserved
